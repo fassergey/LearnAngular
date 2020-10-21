133a53bea052d11eeb8973e41ad6a57a2e2adbc1
Sergey Fasolko <fas.sergey@gmail.com>
2020-10-21 10:55:24 +0300

    reverted changes with Observable

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 
-import { BehaviorSubject } from 'rxjs';
-
 import { Product } from '../../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private products: Map<Product, number> = new Map<Product, number>();
-  private subject: BehaviorSubject<Map<Product, number>> = new BehaviorSubject<Map<Product, number>>(this.products);
-  products$ = this.subject.asObservable();
+  products: Map<Product, number> = new Map<Product, number>();
+
+  get itemsNumber(): number {
+    return this.products && this.products.size > 0 ?
+      this.products.size :
+      0;
+  }
+
+  get sum(): number {
+    return this.products && this.products.size > 0 ?
+      this.calculateSum() :
+      0;
+  }
 
   addToCart(product: Product): void {
     if (this.products.has(product)) {
@@ -19,8 +27,6 @@ export class CartService {
     } else {
       this.products.set(product, 1);
     }
-
-    this.subject.next(this.products);
   }
 
   changeItemNumber(product: Product, difference: number): void {
@@ -31,13 +37,18 @@ export class CartService {
     } else {
       this.products.set(product, value);
     }
-
-    this.subject.next(this.products);
   }
 
   removeItem(product: Product): void {
     this.products.delete(product);
+  }
 
-    this.subject.next(this.products);
+  private calculateSum(): number {
+    let sum = 0;
+    for (const entry of this.products) {
+      sum += entry[0].price * entry[1];
+    }
+
+    return sum;
   }
 }
